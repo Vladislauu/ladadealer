ce74d8a226a9af9e19aac7c789744008f35200cd
Vladislauu <vdotsen@yandex.ru>
2026-04-04 09:14:49 +0300

    burga

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+const burger = document.getElementById("burgerBtn")
+const menu = document.getElementById("navMenu")
+
+function toggleMenu(arg) 
+{
+    burger.classList.toggle('active')
+    menu.classList.toggle('active')
+}
+
+burger.addEventListener('click', toggleMenu)

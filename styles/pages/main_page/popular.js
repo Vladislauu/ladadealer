f5d547f7402399ae4a5589759b98f6be53aa5c42
Vladislauu <vdotsen@yandex.ru>
2026-04-09 18:02:06 +0300

    селектор

--- styles/pages/main_page/popular.js
+++ styles/pages/main_page/popular.js
@@ -3,51 +3,68 @@ const prevBtn = document.getElementById('popular__switch--left')
 const nextBtn = document.getElementById('popular__switch--right')
 const cards = Array.from(document.querySelectorAll('.popular__card'))
 let cardWidth = cards[0].offsetWidth
-let animationMutex = false;
+let animationMutex = false; //cards[Math.floor(cards.length/2)].classList.toggle('active')
+
+function resizeactive()
+{
+  track.children[Math.floor(track.children.length/2)].classList.toggle('active')
+}
 
 function moveNext() {
-  cardWidth = cards[0].offsetWidth
   if (animationMutex) return;
   animationMutex = true;
+  cardWidth = track.children[0].offsetWidth
+  resizeactive()
+  track.addEventListener('transitionend', function onResizeEnd() {
+      track.removeEventListener('transitionend', onResizeEnd);
 
-  track.style.transition = 'transform 0.4s ease-in-out';
-  track.style.transform = `translateX(-${cardWidth}px)`;
+      track.style.transition = 'transform 0.4s ease-in-out'
+      track.style.transform = `translateX(-${cardWidth}px)`;
 
-  track.addEventListener('transitionend', function onTransitionEnd() {
-    track.removeEventListener('transitionend', onTransitionEnd);
-    
-    const firstCard = track.children[0];
-    track.appendChild(firstCard);
-    
-    track.style.transition = 'none';
-    track.style.transform = 'translateX(0)';
-    
-    track.offsetHeight;
-    
-    track.style.transition = '';
-    animationMutex = false;
-  });
+      track.addEventListener('transitionend', function onTransitionEnd() {
+        track.removeEventListener('transitionend', onTransitionEnd);
+        track.style.transition = 'none';
+        track.style.transform = 'translateX(0)';
+        const firstCard = track.children[0];
+        track.appendChild(firstCard);
+
+        track.offsetHeight;
+        resizeactive()
+        track.addEventListener('transitionend', function onTransitionEnd() {
+          track.removeEventListener('transitionend', onTransitionEnd);
+          animationMutex = false;
+        })
+    })
+  })
 }
 
 function movePrev() {
-  cardWidth = cards[0].offsetWidth
   if (animationMutex) return;
   animationMutex = true;
+  cardWidth = track.children[0].offsetWidth
+  resizeactive()
+  track.addEventListener('transitionend', function onResizeEnd() {
+      track.removeEventListener('transitionend', onResizeEnd);
 
-  track.style.transition = 'none';
-  const lastCard = track.children[track.children.length - 1];
-  track.prepend(lastCard);
-  track.style.transform = `translateX(-${cardWidth}px)`;
-  track.offsetHeight;
-  
-  track.style.transition = 'transform 0.4s ease-in-out';
-  track.style.transform = 'translateX(0)';
-  
-  track.addEventListener('transitionend', function onEnd() {
-    track.removeEventListener('transitionend', onEnd);
-    animationMutex = false;
-  });
+      track.style.transition = 'transform 0.4s ease-in-out'
+      track.style.transform = `translateX(${cardWidth}px)`;
+
+      track.addEventListener('transitionend', function onTransitionEnd() {
+        track.removeEventListener('transitionend', onTransitionEnd);
+        const lastCard = track.children[track.children.length - 1];
+        track.prepend(lastCard);
+        track.offsetHeight;
+        track.style.transition = 'none';
+        track.style.transform = 'translateX(0)';
+        track.offsetHeight;
+        resizeactive()
+        track.addEventListener('transitionend', function onTransitionEnd() {
+          track.removeEventListener('transitionend', onTransitionEnd);
+          animationMutex = false;
+        })
+    })
+  })
 }
 
-nextBtn.addEventListener('click', moveNext, cardWidth);
-prevBtn.addEventListener('click', movePrev, cardWidth);
+nextBtn.addEventListener('click', moveNext);
+prevBtn.addEventListener('click', movePrev);
